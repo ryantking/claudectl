aa351a44e76f812a255c56b7e86aebf0c5f05aa5
Ryan King <ryan.king@flipsidecrypto.com>
2025-12-01 09:40:34 -0500

    Update main.py: major changes (123 lines)

--- src/claudectl/cli/main.py
+++ src/claudectl/cli/main.py
@@ -11,6 +11,7 @@ from typing import Annotated
 import typer
 
 from claudectl.cli.commands.hook import app as hook_app
+from claudectl.cli.commands.init import app as init_app
 from claudectl.cli.commands.workspace import app as workspace_app
 from claudectl.cli.output import CLIError, handle_exception, is_json_output, set_json_output
 
